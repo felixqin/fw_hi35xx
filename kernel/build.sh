c625d0a5d90000407221ed690c9c4b2864964821
felix <qfl2000@gmail.com>
2016-07-27 22:37:53 +0800

    add kernel build script

---
+++ kernel/build.sh
@@ -0,0 +1,18 @@
+
+THISDIR=`dirname $0`
+PACKAGE=linux-3.4.y
+
+cd ${THISDIR}
+
+tar zxvf ${PACKAGE}.tgz
+
+cd ${PACKAGE}
+
+cp ../xa3516a_mini_config  .config
+make -j8 ARCH=arm CROSS_COMPILE=arm-hisiv300-linux- uImage
+
+cp arch/arm/boot/uImage ../
+cd ..
+rm -fr ${PACKAGE}
+
+
